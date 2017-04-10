728ff0b687af72b04e23173356254cc135fdbef3
jw <jw@localhost.localdomain>
2017-04-10 11:56:19 +0200

    added grid function etc

--- main.py
+++ main.py
@@ -15,25 +15,15 @@ import numpy as np
 
 
 logger=True
-class testwindow():
-    def __init__(self):
-        self.window = Tkinter.Toplevel()
-        self.window.bind("<Return>", self.apply)
-        self.window.bind("<Escape>", self.cancel)
-        self.window.protocol("WM_DELETE_WINDOW", self.cancel) 
-    def cancel(self, event=None):
-        self.window.destroy()
-    def apply(self, event=None):
-        pass
 
-
-        
 class imagewindow:
     def __init__(self, master):
         self.master = master
         menubar = Tkinter.Menu(master)
         
         self.img = PIL.Image.new('L', (800,500)) #keep here for now
+        self.gimg = PIL.Image.new('L', (800,500)) #keep here for now
+        self.bimg = PIL.Image.new('1', (800,500)) #keep here for now
 
         filemenu = Tkinter.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open", command=self.openfile)
@@ -46,6 +36,8 @@ class imagewindow:
         self.bin_buffer_count = 4
         self.greyscale_buffers = self.gs_buffer_count*[None]
         self.bin_buffers = self.bin_buffer_count*[None]
+
+        self.show_binary = True
 
         savemenu = Tkinter.Menu(menubar, tearoff=0)
         #Would be nice to have this variable but became buggy when attempted
@@ -68,11 +60,15 @@ class imagewindow:
         menubar.add_cascade(label="Load buffer", menu=loadmenu)
 
         menubar.add_command(label="Threshold", command=self.threshold)
+        menubar.add_command(label="Toggle binary", command=self.toggle_binary)
 
         gsops = Tkinter.Menu(menubar, tearoff=0)
         gsops.add_command(label="FIND_EDGES", command=self.edgedetect_find_edges)
         gsops.add_command(label="Sobel", command=self.sobel)
-        gsops.add_command(label="Watersheds", command=lambda : self.select_points(self.watersheds))
+        wshedmenu = Tkinter.Menu(gsops, tearoff=0)
+        wshedmenu.add_command(label="Watershed", command=lambda : self.select_points(self.watershed))
+        wshedmenu.add_command(label="Watershed grid", command=lambda : self.grid(self.watershed))
+        gsops.add_cascade(label="Watershed", menu=wshedmenu)
         menubar.add_cascade(label="GS Operations", menu=gsops)
 
         binops = Tkinter.Menu(menubar, tearoff=0)
@@ -94,10 +90,22 @@ class imagewindow:
         master.mainloop()
 
     def update(self):
-        image1 = PIL.ImageTk.PhotoImage(self.img)
         #updates the current instance
         #there seems to be a recursion issue here
-        self.master.geometry('%dx%d' % (self.img.size[0],self.img.size[1]))
+        colors = self.bimg.convert(mode="RGBA").split()
+        colors[0].paste(self.bimg.point(lambda p: (255*(p>0))))
+        colors[1].paste(self.bimg.point(lambda p: 0))
+        colors[2].paste(self.bimg.point(lambda p: 0))
+        
+        bin = PIL.Image.merge("RGBA", colors) #maybe change to something more reasonable
+        bin.putalpha(self.bimg)
+
+        self.img = self.gimg.convert("RGBA")
+        if self.show_binary:
+            self.img.paste(bin, (0,0), bin)
+        self.master.geometry('%dx%d' % (self.gimg.size[0],self.gimg.size[1]))
+        
+        image1 = PIL.ImageTk.PhotoImage(self.img)
         self.label_image.configure(image = image1)
         self.label_image.image = image1
 
@@ -106,16 +114,25 @@ class imagewindow:
         self.img = self.img.convert('L')
         self.update()
 
+    def toggle_binary(self):
+        if self.show_binary==1:
+            self.show_binary=0
+        else:
+            self.show_binary=1
+        self.update()
+
     def openfile(self):
         filename = tkFileDialog.askopenfilename()
-        self.img = PIL.Image.open(filename).convert("L")
+        self.img = PIL.Image.open(filename).convert("L") #remove?
+        self.gimg = PIL.Image.open(filename).convert("L")
+        self.bimg = PIL.Image.new(mode="1", size=self.gimg.size)
         self.orig = self.img.copy()
         print 'img = PIL.Image.open("' + filename + '").convert("L")'
         print 'orig = img.copy()'
         self.update()
 
     def load_original(self):
-        self.img = self.orig
+        self.gimg = self.orig
         self.update()
 
     def savefile(self):
@@ -125,8 +142,8 @@ class imagewindow:
 
     def select_points(self,fn):
         #Permits the user to interactively select points which are then returned as a list
-        img_copy = self.img.copy()
-        self.img = self.img.convert(mode="RGB")
+        img_copy = self.gimg.copy()
+        self.gimg = self.gimg.convert(mode="RGB")
         point_list = []
         def point_self(all=None):
             r = 2
@@ -134,7 +151,7 @@ class imagewindow:
                 start=0
             else:
                 start=-1
-            draw = PIL.ImageDraw.Draw(self.img)
+            draw = PIL.ImageDraw.Draw(self.gimg)
             for i in point_list[start:]:
                 draw.ellipse((i[0]-r, i[1]-r, i[0]+r, i[1]+r), fill=(0,255,0,0))
             self.update()
@@ -147,14 +164,14 @@ class imagewindow:
         def undo(self):
             if point_list!=[]:
                 point_list.pop()
-            self.img = img_copy.convert(mode="RGB")
+            self.gimg = img_copy.convert(mode="RGB")
             point_self(1)
             self.update()
 
         def done(event=None):
             root.unbind("<ButtonPress-1>")
             root.unbind("<ButtonPress-3>")
-            self.img = img_copy.copy()
+            self.gimg = img_copy.copy()
             select_window.destroy()
             fn(point_list)
 
@@ -174,8 +191,10 @@ class imagewindow:
         # add thresholding to this
         # would be nice to have better interactivity
         # not sure what option would be best for that
-        old = self.img.copy()
-        hist_img = visual_histogram(self.img)
+        #old = self.gimg.copy()
+        old = self.gimg.copy()
+        self.bimg = PIL.Image.new('1', self.gimg.size)
+        hist_img = visual_histogram(self.gimg)
         
         self.mval = 0 #think about better solution here
         self.uval = 256
@@ -186,7 +205,7 @@ class imagewindow:
         def cancel(event=None):
             #reverts to the old image and closes the histogram prompt
             histogram_window.destroy()
-            self.img=old
+            self.gimg=old
             self.update()
         def click(event=None, mv=None):
             #shows red binary image superimposed on input greyscale image
@@ -197,7 +216,7 @@ class imagewindow:
             elif mv==1:
                 # this gives the default self.mval (cut-off value) as the maximum value position in the histogram
                 # this isn't ideal but it's better than no default value at all
-                self.mval = self.img.histogram().index(max(self.img.histogram()))
+                self.mval = self.gimg.histogram().index(max(self.gimg.histogram()))
             colors = old.convert(mode="RGBA").split()
             colors[0].paste(old.point(lambda p: (255*(p>self.mval)*(p<self.uval))))
             colors[1].paste(old.point(lambda p: 0))
@@ -205,11 +224,12 @@ class imagewindow:
             
             self.bin = PIL.Image.merge("RGBA", colors) #maybe change to something more reasonable
             self.bin.putalpha(old.point(lambda p: (255*(p>self.mval)*(p<self.uval))))
-            self.img = PIL.Image.blend(self.bin, old.convert(mode="RGBA"), 0.5)
+            self.gimg = PIL.Image.blend(self.bin, old.convert(mode="RGBA"), 0.5)
             himg = visual_histogram(old, self.mval, self.uval)
             image1 = PIL.ImageTk.PhotoImage(himg)
             label_image.configure(image = image1)
             label_image.image = image1
+            self.show_binary = True # This is preferred
             self.update()
 
         def rightclick(event):
@@ -220,7 +240,8 @@ class imagewindow:
 
         def apply(event=None):
             #applies changes and saves the binary image in the primary buffer
-            self.img = self.bin.convert(mode='L').point(lambda p: 255*(p>0)).convert(mode='1')
+            self.bimg = self.bin.convert(mode='L').point(lambda p: 255*(p>0)).convert(mode='1')
+            self.gimg = old
             if logger:
                 print ""
                 #change later
@@ -250,29 +271,29 @@ class imagewindow:
     def save_gs_buffer(self, slot):
         #save primary buffer (img) in greyscale buffer number (slot - int)
         #print slot
-        if self.img.mode=="L":
-            self.greyscale_buffers[slot] = self.img.copy()
+        if self.gimg.mode=="L":
+            self.greyscale_buffers[slot] = self.gimg.copy()
             print "# Saved in greyscale slot " + str(slot)
             if logger:
-                print "img" + str(slot)  + " = img.copy()"
+                print "gimg" + str(slot)  + " = gimg.copy()"
         else:
             print "# Not a greyscale image"
 
     def load_gs_buffer(self, slot):
         #load greyscale buffer number (slot - int) into primary buffer (img)
         if self.greyscale_buffers[slot]!=None:
-            self.img = self.greyscale_buffers[slot]
+            self.gimg = self.greyscale_buffers[slot]
             print "# Loaded greyscale slot " + str(slot)
             if logger:
-                print "img = img" + str(slot)
+                print "gimg = gimg" + str(slot)
             self.update()
         else:
             print "# greyscale buffer " + str(slot) + " is empty"
 
     def save_bin_buffer(self, slot):
         #save primary buffer (img) in binary buffer number (slot - int)
-        if self.img.mode=="1":
-            self.bin_buffers[slot] = self.img.copy()
+        if self.bimg.mode=="1":
+            self.bin_buffers[slot] = self.bimg.copy()
             print "# Saved in greyscale slot " + str(slot)
             if logger:
                 print "binimg" + str(slot)  + " = img.copy()"
@@ -282,7 +303,7 @@ class imagewindow:
     def load_bin_buffer(self, slot):
         #load binary buffer number (slot - int) into primary buffer (img)
         if self.bin_buffers[slot]!=None:
-            self.img = self.bin_buffers[slot]
+            self.bimg = self.bin_buffers[slot]
             print "# Loaded binary slot " + str(slot)
             if logger:
                 print "img = binimg" + str(slot)
@@ -291,33 +312,55 @@ class imagewindow:
             print "# binary buffer " + str(slot) + " is empty"
 
     def edgedetect_find_edges(self):
-        self.img = self.img.filter(PIL.ImageFilter.FIND_EDGES)
+        self.gimg = self.gimg.filter(PIL.ImageFilter.FIND_EDGES)
         self.update()
 
     def sobel(self):
-        tmpdata = scipy.ndimage.sobel(self.img)
-        self.img.putdata(tmpdata.flatten())
+        tmpdata = scipy.ndimage.sobel(self.gimg)
+        self.gimg.putdata(tmpdata.flatten())
         self.update()
 
-    def watersheds(self, point_list):
-        markers = np.zeros(self.img.size).astype(np.int16)
+    def grid(self, fn):
+        # runs the function with n by n grid, n supplied by user
+        top = Tkinter.Toplevel(height=300, width=300)
+        e = Tkinter.Entry(top)
+        e.insert(Tkinter.END, "5")
+        e.pack()
+        e.focus_set()
+        def construct_run(n): #this can probably be simplified
+            xpts = [int((0.5+i)*self.img.size[0]/n) for i in range(n)]
+            ypts = [int((0.5+i)*self.img.size[1]/n) for i in range(n)]
+            points = n*n*[0]
+            for i,a in enumerate(xpts):
+                for j,b in enumerate(ypts):
+                    points[i*n+j] = (a,b)
+            fn(points)
+            top.destroy()
+        
+        apply = Tkinter.Button(top, text="apply", width=10, 
+                command=lambda : construct_run(int(e.get())))
+        apply.pack()
+        
+        
+    def watershed(self, point_list):
+        markers = np.zeros(self.gimg.size).astype(np.int16)
         factor = 256/len(point_list) # just for looks and easier use
         for i,a in enumerate(point_list):
             markers[a[0],a[1]] = i*factor
-        imin = np.array(self.img).transpose()
+        imin = np.array(self.gimg).transpose()
         imgdata = scipy.ndimage.measurements.watershed_ift(np.array(imin), markers)
-        img2 = PIL.Image.new(mode="L", size=self.img.size)
+        img2 = PIL.Image.new(mode="L", size=self.gimg.size)
         img2.putdata(imgdata.transpose().flatten())
         if logger:
             print "points = " + str(point_list)
             print "# ATTENTION REQUIRED - definition on markers missing"
             print "data = scipy.ndimage.measurements.watershed_ift(np.array(" +str(imin) + "), markers)"
-        self.img = img2.copy()
+        self.gimg = img2.copy()
         self.update()
 
     def invert_bin(self):
-        if self.img.mode=="1":
-            self.img = PIL.ImageChops.invert(self.img)
+        if self.bimg.mode=="1":
+            self.bimg = PIL.ImageChops.invert(self.bimg)
             self.update()
         else:
             print "Not a binary image"
@@ -331,7 +374,7 @@ class imagewindow:
         def AND():
             num = int(e.get())
             if self.bin_buffers!=None:
-                self.img = PIL.ImageChops.logical_and(self.img,self.bin_buffers[num])
+                self.bimg = PIL.ImageChops.logical_and(self.img,self.bin_buffers[num])
                 if logger:
                     print "img = (img and binimg" + str(num) + ")"
             top.destroy()
@@ -339,7 +382,7 @@ class imagewindow:
         def OR():
             num = int(e.get())
             if self.bin_buffers!=None:
-                self.img = PIL.ImageChops.logical_or(self.img,self.bin_buffers[num])
+                self.bimg = PIL.ImageChops.logical_or(self.img,self.bin_buffers[num])
                 if logger:
                     print "img = (img or binimg" + str(num) + ")"
             top.destroy()
@@ -347,9 +390,9 @@ class imagewindow:
         def XOR():
             num = int(e.get())
             if self.bin_buffers!=None:
-                tmp1 = PIL.ImageChops.subtract(self.img, self.bin_buffers[num])
-                tmp2 = PIL.ImageChops.subtract(self.bin_buffers[num], self.img)
-                self.img = PIL.ImageChops.logical_or(tmp1, tmp2)
+                tmp1 = PIL.ImageChops.subtract(self.bimg, self.bin_buffers[num])
+                tmp2 = PIL.ImageChops.subtract(self.bin_buffers[num], self.bimg)
+                self.bimg = PIL.ImageChops.logical_or(tmp1, tmp2)
                 if logger:
                     print "tmp1 = PIL.ImageChops.subtract(img, binimg" + str(num) + ")"
                     print "tmp2 = PIL.ImageChops.subtract(binimg" + str(num) + ", img)"
@@ -366,22 +409,22 @@ class imagewindow:
 
     def morph(self, count, top, morph_type):
         #Should change to non-square shape for nicer binary images
-        img2 = PIL.Image.new(mode="1", size = self.img.size)
+        img2 = PIL.Image.new(mode="1", size = self.bimg.size)
         #convertion to "L" was necessary for some reason
         if morph_type=="erosion":
-            tmp = scipy.ndimage.binary_erosion(self.img.convert("L"), 
+            tmp = scipy.ndimage.binary_erosion(self.bimg.convert("L"), 
                     structure=unit_disk(count))
         elif morph_type=="dilation":
-            tmp = scipy.ndimage.binary_dilation(self.img.convert("L"), 
+            tmp = scipy.ndimage.binary_dilation(self.bimg.convert("L"), 
                     structure=unit_disk(count))
         elif morph_type=="opening":
-            tmp = scipy.ndimage.binary_opening(self.img.convert("L"), 
+            tmp = scipy.ndimage.binary_opening(self.bimg.convert("L"), 
                     structure=unit_disk(count))
         elif morph_type=="closing":
-            tmp = scipy.ndimage.binary_closing(self.img.convert("L"), 
+            tmp = scipy.ndimage.binary_closing(self.bimg.convert("L"), 
                     structure=unit_disk(count))
         elif morph_type=="fill_holes":
-            tmp = scipy.ndimage.binary_fill_holes(self.img.convert("L"))
+            tmp = scipy.ndimage.binary_fill_holes(self.bimg.convert("L"))
 
         if logger:
             print 'tmpimg = PIL.Image.new(mode="1", size = img.size)'
@@ -393,7 +436,7 @@ class imagewindow:
 
         img2.putdata(255*tmp.flatten())
         top.destroy()
-        self.img = img2.convert("1")
+        self.bimg = img2.convert("1")
         self.update()
 
 
